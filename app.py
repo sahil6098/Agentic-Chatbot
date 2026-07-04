a750810b03d3f7ca3ed516a12ce425c7bc5e1e95
Sahil Sawant <sahilsawant7120@gmail.com>
2026-07-04 22:14:22 +0530

    Added streaming in response

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ for message in st.session_state["message_history"]:
 user_input = st.chat_input("Type your message here...")
 
 if user_input:
+    # Display user message
     st.session_state["message_history"].append(
         {"role": "user", "content": user_input}
     )
@@ -28,16 +29,22 @@ if user_input:
     with st.chat_message("user"):
         st.write(user_input)
 
-    response = chatbot.invoke(
-        {"messages": [HumanMessage(content=user_input)]},
-        config=CONFIG
-    )
+    # Assistant message with streaming
+    with st.chat_message("assistant"):
 
-    ai_message = response["messages"][-1].content
+        placeholder = st.empty()
+        ai_message = ""
 
+        for event in chatbot.stream(
+            {"messages": [HumanMessage(content=user_input)]},
+            config=CONFIG,
+            stream_mode="values"
+        ):
+            if "messages" in event:
+                ai_message = event["messages"][-1].content
+                placeholder.markdown(ai_message)
+
+    # Save final response
     st.session_state["message_history"].append(
         {"role": "assistant", "content": ai_message}
     )
-
-    with st.chat_message("assistant"):
-        st.write(ai_message)
