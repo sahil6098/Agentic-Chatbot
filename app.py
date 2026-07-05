d75450af98ee8d4ac6f5c74ba8537ca18c35433a
Sahil Sawant <sahilsawant7120@gmail.com>
2026-07-05 22:56:33 +0530

    Added threading for chatbot

--- app.py
+++ app.py
@@ -1,50 +1,42 @@
+from agentic_chatbot_backend import chatbot 
+from langchain_core.messages import BaseMessage, HumanMessage
 import streamlit as st
-from agentic_chatbot_backend import chatbot
-from langchain_core.messages import HumanMessage
 
 st.title("Agentic Chatbot with LangGraph")
 
-CONFIG = {
-    "configurable": {
-        "thread_id": "thread_1"
-    }
-}
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
-if "message_history" not in st.session_state:
-    st.session_state["message_history"] = []
 
-# Display previous messages
-for message in st.session_state["message_history"]:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
-user_input = st.chat_input("Type your message here...")
+
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+
+user_input = st.chat_input('Type here')
 
 if user_input:
-    # Display user message
-    st.session_state["message_history"].append(
-        {"role": "user", "content": user_input}
-    )
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+    
 
-    with st.chat_message("user"):
-        st.write(user_input)
+    # first add the message to message_history
+    with st.chat_message('assistant'):
 
-    # Assistant message with streaming
-    with st.chat_message("assistant"):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+        )
 
-        placeholder = st.empty()
-        ai_message = ""
-
-        for event in chatbot.stream(
-            {"messages": [HumanMessage(content=user_input)]},
-            config=CONFIG,
-            stream_mode="values"
-        ):
-            if "messages" in event:
-                ai_message = event["messages"][-1].content
-                placeholder.markdown(ai_message)
-
-    # Save final response
-    st.session_state["message_history"].append(
-        {"role": "assistant", "content": ai_message}
-    )
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
